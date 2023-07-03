060226ca549e02ed10abaf32f68f285035d3b113
marszman <136658905+marszman@users.noreply.github.com>
2023-07-03 16:48:38 +0000

    secret message - initial commit

--- secret_encoder.rb
+++ secret_encoder.rb
@@ -5,5 +5,21 @@ secret = [
 ].sample
 pp secret
 # write your program below
+encoded_message = ""
 character_array = secret.split("")
 character_array.each_with_index do |character, index|
+  character
+  if character.downcase == "a"
+    character = "1"
+  elsif character.downcase == "e"
+    character = "2"
+  elsif character.downcase == "i"
+    character = "3"
+  elsif character.downcase == "o"
+    character = "4"
+  elsif character.downcase == "u"
+    character = "5"
+  end
+  encoded_message = encoded_message + character
+end
+pp encoded_message
