5e761e2c329b6e1aef0f80615869bb092534374a
marszman <136658905+marszman@users.noreply.github.com>
2023-07-03 16:33:54 +0000

    leap year complete

--- leap_year.rb
+++ leap_year.rb
@@ -6,7 +6,7 @@ years = [
 year = years.sample
 # write your program below
 if (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
-  pp year.to_s + " is a leap year!""
+  pp year.to_s + " is a leap year!"
 else
   pp year.to_s + " is not a leap year."
 end
