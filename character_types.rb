2ea486d46da13cfb2e7f934976b168d0feadcde9
marszman <136658905+marszman@users.noreply.github.com>
2023-07-03 17:33:42 +0000

    character types - complete

--- character_types.rb
+++ character_types.rb
@@ -10,6 +10,6 @@ no_spaces = string.length - string.gsub(" ","").length
 no_numbers = string.gsub(/[^0-9]/,"").length
 no_letters = string.gsub(/[^a-z]/i,"").length
 
-pp "Number of letters in the string is: " + no_spaces
-pp "Number of spaces in the string is: " + no_numbers
-pp "Number of digits in the string is: " + no_letters
+pp "Number of letters in the string is: " + no_letters.to_s
+pp "Number of spaces in the string is: " + no_spaces.to_s
+pp "Number of digits in the string is: " + no_numbers.to_s
