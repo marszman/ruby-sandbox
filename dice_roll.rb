f872c16716a7ab25caa2d1164c4c8c0c3219e258
marszman <136658905+marszman@users.noreply.github.com>
2023-07-03 16:20:06 +0000

    dice - update

--- dice_roll.rb
+++ dice_roll.rb
@@ -1,7 +1,7 @@
-dice = rand(1..6)
 guess = rand(1..6)
-if dice == guess
+roll = rand(1..6)
+if guess == roll
   pp "You guessed correctly!"
 else
-  pp "Sorry, you guessed " + guess.to_s + ". The die landed on " + dice.to_s + "."
+  pp "Sorry, you guessed " + guess.to_s + ". The die landed on " + roll.to_s + "."
 end
