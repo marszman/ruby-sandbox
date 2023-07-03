66934e0e171e793a5ea927c71b6a83490a6b8fe3
marszman <136658905+marszman@users.noreply.github.com>
2023-07-03 17:31:01 +0000

    character types - update

--- character_types.rb
+++ character_types.rb
@@ -6,6 +6,10 @@ strings = [
 string = strings.sample
 pp string
 # write your program below
-pp no_space = string.length - string.gsub(" ","").length
-pp no_numbers = string.gsub(/[^0-9]/,"").length
-pp no_letters = string.gsub(/[^a-z]/i,"").length
+no_spaces = string.length - string.gsub(" ","").length
+no_numbers = string.gsub(/[^0-9]/,"").length
+no_letters = string.gsub(/[^a-z]/i,"").length
+
+pp "Number of letters in the string is: " + no_spaces
+pp "Number of spaces in the string is: " + no_numbers
+pp "Number of digits in the string is: " + no_letters
